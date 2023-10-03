336fab305d06206f1f2b2aaa8d294ba966e17639
him-rane <himanshurane6501@gmail.com>
2023-10-03 14:36:01 +0530

    Automate Update of ChromeDrivers, Restore Locator Files

---
+++ locators.py
@@ -0,0 +1,44 @@
+# Menues
+DashboardMenu='//*[@id="mainMenu1"]',"#mainMenu1","mainMenu1"
+StatusMenu='//*[@id="mainMenu2"]','#mainMenu2',"mainMenu2"
+NetworkMenu='//*[@id="mainMenu3"]',"#mainMenu3","mainMenu3"
+SecurityMenu='//*[@id="mainMenu4"]',"#mainMenu4","mainMenu4"
+AdministrationMenu='//*[@id="mainMenu5"]','#mainMenu5',"mainMenu5"
+AdvancedMenu='//*[@id="mainMenu6"]','#mainMenu6',"mainMenu6"
+
+
+#Administration >> User>>
+AdministrationMenu_UserSubMenu= '//*[@id="tf1_administration_users"]', "#tf1_administration_users", "tf1_administration_users"
+
+AdministrationMenu_UserSubMenu_Row1Col2='//*[@id="users2"]/td[2]',"tr[id='users2'] td:nth-child(2)"
+AdministrationMenu_UserSubMenu_Row1Col1='//*[@id="users2"]/td[1]',"tr[id='users2'] td[class='gradeA sorting_1']"
+
+AdministrationMenu_UserSubMenu_Row2Col2='//*[@id="users1"]/td[2]',"tr[id='users1'] td:nth-child(2)"
+AdministrationMenu_UserSubMenu_Row2Col1='//*[@id="users1"]/td[1]',"tr[id='users1'] td[class='gradeA sorting_1']"
+
+AdministrationMenu_UsersConfiguration_Pass='//*[@id="tf1_txtPwd"]','#tf1_txtPwd','tf1_txtPwd'
+AdministrationMenu_UsersConfiguration_CfmPass='//*[@id="tf1_txtCfmPwd"]','#tf1_txtCfmPwd','tf1_txtCfmPwd'
+AdministrationMenu_UsersConfiguration_SaveBtn='//*[@id="tf1_dialog"]/div[3]/input[2]',"input[value='Save']"
+
+#Login Page
+LoginPage_UserName= '//*[@id="tf1_userName"]', '#tf1_userName', 'tf1_userName'
+LoginPage_Password= '//*[@id="tf1_password"]', '#tf1_password', 'tf1_password'
+LoginPage_LoginButton= "//button[normalize-space()='Login']", "button[title='Login']"
+
+
+firmware_version_sidebar = "/html/body/div[1]/div[1]/div[2]/p[1]/span", "body > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > p:nth-child(1) > span:nth-child(2)"
+mac_address_daseboard = '//*[@id="main"]/div[6]/div[2]/div[4]/div[3]/div[4]/p', "body > div:nth-child(1) > div:nth-child(2) > div:nth-child(19) > div:nth-child(3) > div:nth-child(4) > div:nth-child(3) > div:nth-child(4) > p:nth-child(2)"
+gpon_status_WAN_Information = '//*[@id="main"]/div[6]/div[1]/div/div[17]/p', "body > div:nth-child(1) > div:nth-child(2) > div:nth-child(19) > div:nth-child(2) > div:nth-child(1) > div:nth-child(18) > p:nth-child(2)"
+ipv4_address_daseboard = '//*[@id="main"]/div[6]/div[2]/div[4]/div[3]/div[5]/p', "body > div:nth-child(1) > div:nth-child(2) > div:nth-child(19) > div:nth-child(3) > div:nth-child(4) > div:nth-child(3) > div:nth-child(5) > p:nth-child(2)"
+
+ipv6_address_daseboard_1 = '//*[@id="main"]/div[6]/div[2]/div[4]/div[3]/div[7]/p[1]', 'body > div:nth-child(1) > div:nth-child(2) > div:nth-child(19) > div:nth-child(3) > div:nth-child(4) > div:nth-child(3) > div:nth-child(7) > p:nth-child(2)'
+ipv6_address_daseboard_2 = '//*[@id="main"]/div[6]/div[2]/div[4]/div[3]/div[7]/p[2]', 'body > div:nth-child(1) > div:nth-child(2) > div:nth-child(19) > div:nth-child(3) > div:nth-child(4) > div:nth-child(3) > div:nth-child(7) > p:nth-child(3)'
+
+#Administration >> Maintenance >>
+# Maintenance '//*[@id="tf1_administration_backupRestore"]','#tf1_administration_backupRestore',
+Maintenance_BackupReboot_RebootButton = '//*[@id="tf1_frmBackupReboot"]/div[1]/div/input', "input[title='Reboot']"
+Maintenance_BackupReboot_DefaultButton = '//*[@id="tf1_frmBackupFactoryDefaultSettings"]/div[1]/div/input', "input[title='Default']"
+
+DateTimeConfiguration_CurrentRouterTime = '//*[@id="tf1__div"]/p', "div[id='tf1__div'] p"
+DateTimeConfiguration_TimeZone = '//*[@id="tf1_selTimezone"]', "#tf1_selTimezone"
+DateTimeConfiguration_SaveButton = '//*[@id="tf1_frmDateAndTime"]/div[10]/input[1]', "input[title='Save']"
--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-# from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 from logger_util import logger
 
 
 from selenium import webdriver
+from selenium import webdriver
+
 
 options = webdriver.ChromeOptions()
 options.add_argument('ignore-certificate-errors')
@@ -15,8 +17,13 @@ class Setup:
         logger.debug("Initializing Chrome WebDriver")
 
         try:
-            # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-            driver = webdriver.Chrome()
+            driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+            # from selenium import webdriver
+
+            # Correct way to initialize a WebDriver (e.g., for Chrome)
+            # driver = webdriver.Chrome('C:\\Users\\ontvi\\OneDrive\\Desktop\\chromedriver-win64\\chromedriver.exe')
+
+            # driver = webdriver.Chrome("")
             driver.implicitly_wait(15)
             driver.maximize_window()
             driver.get("http://192.168.29.1")
